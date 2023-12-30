1c9ca7930548d4bfeaf5a2778b14656218a0373f
Kristof Kox <kristof.kox@ravago.com>
2023-12-30 14:57:01 +0100

    Code Split and Lazy-Load at the Component Lev

--- src/app/admin/admin.routes.ts
+++ src/app/admin/admin.routes.ts
@@ -1,15 +1,25 @@
 import {Routes} from "@angular/router";
 
-// containers
-import {DonutListComponent} from './containers/donut-list/donut-list.component';
-import {DonutSingleComponent} from './containers/donut-single/donut-single.component';
 
-// components
 
 export const AdminRoutes: Routes = [
-  {path: 'donuts', component: DonutListComponent},
-  {path: 'donuts/new', component: DonutSingleComponent, data: {isEdit: false}},
-  {path: 'donuts/:id', component: DonutSingleComponent, data: {isEdit: true}},
+  {
+    path: 'donuts',
+    loadComponent: () =>
+      import('../admin/containers/donut-list/donut-list.component').then(
+        (x) => x.DonutListComponent
+      ),
+  },
+  {path: 'donuts/new', loadComponent: () =>
+      import('../admin/containers/donut-single/donut-single.component').then(
+        (x) => x.DonutSingleComponent
+      ),
+    data: {isEdit: false}},
+  {path: 'donuts/:id', loadComponent: () =>
+      import('../admin/containers/donut-single/donut-single.component').then(
+        (x) => x.DonutSingleComponent
+      ),
+    data: {isEdit: true}},
   // redirects http://localhost:4200/admin to http://localhost:4200/admin/donuts
   // put always at the end
   {path: '', pathMatch: 'full', redirectTo: 'donuts'},
